4f6526b4c435092b8599d93d758fa8270dacbdea
FilipMantaj <filip.mantaj17@o2.pl>
2024-05-27 22:20:12 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { App } from 'components/App';
 import { store } from './redux/store';
 import './index.css';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
